0b358105af06058f2b3a2127c6dcdc39cbf4c340
ozwaldorf <self@ossian.dev>
2024-02-13 18:50:32 -0500

    feat: fetch github/twitter text records

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
 import { Web3 } from 'web3';
+import { namehash } from './utils.js';
 
-BigInt.prototype.toJSON = function() { return this.toString() }
+const web3 = new Web3('https://cloudflare-eth.com');
 
+export const main = async (name = "ens.eth") => {
+  const resolver = await web3.eth.ens.getResolver(name);
+  const hash = namehash(name);
 
-export async function main(name = "ens.eth") {
-  const web3 = new Web3('https://cloudflare-eth.com');
-  const address = await web3.eth.ens.getAddress(name);
-  const eth_balance = await web3.eth.getBalance(address);
+  // TODO: Batch these contract calls
+  const eth_address = await resolver.methods.addr(hash, 60).call(); // 60 = ETH
+  const github = await resolver.methods.text(hash, 'com.github').call();
+  const twitter = await resolver.methods.text(hash, 'com.twitter').call();
+
+  const eth_balance = await web3.eth.getBalance(eth_address);
 
   return `<html>
   <body>
     <h1>${name}</h1>
-    <p>Address: ${address}</p>
+    <p>Address: ${eth_address}</p>
     <p>Balance: ${eth_balance} Wei</p>
+    <p>Github: @${github}</p>
+    <p>Twitter: @${twitter}</p>
   </body>
 </html>`;
 };
---
+++ src/utils.js
@@ -0,0 +1,25 @@
+// Namehash utility: https://github.com/web3/web3.js/blob/4.x/packages/web3-eth-ens/src/utils.ts
+
+import { utils } from 'web3';
+// eslint-disable-next-line camelcase
+import { ens_normalize } from '@adraffy/ens-normalize';
+
+export const namehash = (inputName) => {
+  // Reject empty names:
+  let node = '';
+  for (let i = 0; i < 32; i += 1) {
+    node += '00';
+  }
+
+  if (inputName) {
+    const name = ens_normalize(inputName);
+    const labels = name.split('.');
+
+    for (let i = labels.length - 1; i >= 0; i -= 1) {
+      const labelSha = utils.sha3Raw(labels[i]).slice(2);
+      node = utils.sha3Raw(`0x${node}${labelSha}`).slice(2);
+    }
+  }
+
+  return `0x${node}`;
+};
