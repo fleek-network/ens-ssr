34cbcb5dd24180e627a2649e57bb87276939f1b0
ozwaldorf <self@ossian.dev>
2024-02-13 23:12:20 -0500

    chore: format and comments

--- src/index.js
+++ src/index.js
@@ -1,26 +1,33 @@
-import { Web3Eth } from 'web3-eth';
-import { ENS } from 'web3-eth-ens';
-import { fromWei } from 'web3-utils';
-import { namehash } from './utils.js';
+import { Web3Eth } from "web3-eth";
+import { ENS } from "web3-eth-ens";
+import { fromWei } from "web3-utils";
+import { namehash } from "./utils.js";
 
-const eth = new Web3Eth('https://cloudflare-eth.com');
-const ens = new ENS(undefined, 'https://cloudflare-eth.com');
+const eth = new Web3Eth("https://cloudflare-eth.com");
+const ens = new ENS(undefined, "https://cloudflare-eth.com");
 
 export const main = async (name = "ens.eth") => {
+  // Get the resolver contract for the name
   const resolver = await ens.getResolver(name);
   const hash = namehash(name);
 
-  // TODO: Batch these contract calls
-  const [[eth_address, eth_balance], github, twitter, avatar] = await Promise.all([
+  // Fetch all the needed data from the resolver contract in parallel
+  const [
+    [eth_address, eth_balance],
+    github,
+    twitter,
+    avatar,
+  ] = await Promise.all([
     resolver.methods.addr(hash, 60).call().then(async (address) => {
-      const balance = fromWei(await eth.getBalance(address), 'ether');
+      const balance = fromWei(await eth.getBalance(address), "ether");
       return [address, balance];
     }),
-    resolver.methods.text(hash, 'com.github').call(),
-    resolver.methods.text(hash, 'com.twitter').call(),
-    resolver.methods.text(hash, 'avatar').call()
+    resolver.methods.text(hash, "com.github").call(),
+    resolver.methods.text(hash, "com.twitter").call(),
+    resolver.methods.text(hash, "avatar").call(),
   ]);
 
+  // Render final html
   return `<html>
   <body>
     <image src="${avatar}" width="150px"/>
