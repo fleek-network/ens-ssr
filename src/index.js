ec2bb7471a50ca8384913f9f37bcf0cb7936950c
ozwaldorf <self@ossian.dev>
2024-02-13 22:58:09 -0500

    feat: parse ether value correctly for display

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { Web3Eth } from 'web3-eth';
 import { ENS } from 'web3-eth-ens';
+import { fromWei } from 'web3-utils';
 import { namehash } from './utils.js';
 
 const eth = new Web3Eth('https://cloudflare-eth.com');
@@ -12,7 +13,7 @@ export const main = async (name = "ens.eth") => {
   // TODO: Batch these contract calls
   const [[eth_address, eth_balance], github, twitter, avatar] = await Promise.all([
     resolver.methods.addr(hash, 60).call().then(async (address) => {
-      const balance = await eth.getBalance(address);
+      const balance = fromWei(await eth.getBalance(address), 'ether');
       return [address, balance];
     }),
     resolver.methods.text(hash, 'com.github').call(),
@@ -25,7 +26,7 @@ export const main = async (name = "ens.eth") => {
     <image src="${avatar}" width="150px"/>
     <h1>${name}</h1>
     <p>Address: ${eth_address}</p>
-    <p>Balance: ${eth_balance} Wei</p>
+    <p>Balance: ${eth_balance}</p>
     <p>Github: ${github}</p>
     <p>Twitter: ${twitter}</p>
   </body>
